99d2e8ce1fbcbc306f4b497764570c11538eb9b9
Tom Nguyen <tomtnguyen88@gmail.com>
2023-06-10 16:26:42 -0700

    lets users write a hack, choose a category, and send post req

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './components/Login'
+import NavBar from './components/NavBar'
+import HackCreator from './components/HackCreator'
+import MainDisplay from './components/MainDisplay'
+
 
 const App = () => {
+
+  // const [value, setValue] = React.useState(['Categories', 'Codesmith', 'Time', 'Money']);
+
   return (
-    <div>App</div>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={Login} />
+        {/* <Route path="/base" component={MainDisplay} />
+        <Route path="/create" component={CreateHack} />
+        <Route path="/categories" component={Login} />  */}
+      </Switch>
+      <HackCreator />
+    </Router>
     // <mainContainer />
   )
 }
 
 export default App
+
+/*
+      
+      
+      
+      
+*/
